0935d1934b8ee586a5a53b684a2934bfb4810918
PumaDev310 <eriklevinn@outlook.com>
2021-05-04 23:25:37 -0700

    origin

---
+++ README.md
@@ -0,0 +1,229 @@
+### Looking for someone to take over the project as I've recently changes jobs and had a child and am no longer capable of supporting this dependency. If anyone is interested, please feel free to take ownership of it
+
+React Coverflow
+===
+[![npm version](https://badge.fury.io/js/react-coverflow.svg)](http://badge.fury.io/js/react-coverflow)
+
+[![NPM](https://nodei.co/npm/react-coverflow.png)](https://nodei.co/npm/react-coverflow/)
+
+---
+
+![](https://raw.githubusercontent.com/andyyou/react-coverflow/gh-pages/images/preview.png)
+
+React Coverflow is a React component for building cover flow style carousel in a convenient way.
+
+Features of `react-coverflow`
+
+* Flexbox styles of CSS 3.
+* Support scroll in the component.
+* Support navigation buttons optional
+* Using css-module
+* Support mobile
+
+## Getting started
+
+Install `react-coverflow` using npm.
+
+```
+$ npm install react-coverflow
+```
+
+The required stylesheet using `css-module` and include in package(js file), so you don't need include other stylesheet.
+
+## Usage
+
+```js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Coverflow from 'react-coverflow';
+
+const fn = function () {
+  /* do your action */
+}
+
+ReactDOM.render(
+  <Coverflow width="960" height="500" classes={{background: 'rgb(233, 23, 23)'}} className=''
+    displayQuantityOfSide={2}
+    navigation={false}
+    enableScroll={true}
+    clickable={true}
+    active={0}
+  >
+    <div
+      onClick={() => fn()}
+      onKeyDown={() => fn()}
+      role="menuitem"
+      tabIndex="0"
+    >
+      <img
+        src='image/path'
+        alt='title or description'
+        style={{
+          display: 'block',
+          width: '100%',
+        }}
+      />
+    </div>
+    <img src='image/path' alt='title or description' data-action="http://andyyou.github.io/react-coverflow/"/>
+    <img src='image/path' alt='title or description' data-action="http://andyyou.github.io/react-coverflow/"/>
+  </Coverflow>,
+
+  document.querySelector('.content')
+);
+```
+
+In order to pass functions to the images, you can simply wrap the `<img />` in a `<div>`. You should make sure to give your img specific styling properties to confine it to the parent div.
+
+For more examples, on how to use React Coverflow, please visit http://andyyou.github.io/react-coverflow/
+
+#### Properties
+| Name                 | Type      | Default           | Description                                  |
+|----------------------|-----------|-------------------|----------------------------------------------|
+| children             | node      |                   | should be `<img />` nodes                    |
+| infiniteScroll       | boolean   | false             | Allows the carousel to scroll from the last image to the (upon clicking the next button) or from the first to the last (by clicking the previous button). infiniteScroll is not supported by mouse scrolling. |
+| displayQuantityOfSide| number    |                   | The number of display image from center to the one side end.|
+| navigation           | boolean   | false             | Enable navigation arrows. This will disable image-click navigation.      |
+| enableHeading        | boolean   | true              | Shows the img alt as the label for the img.  |
+| enableScroll         | boolean   | true              | Enable scrolling feature.                    |
+| media                | object    |                   | To support media query, if you want responsive with parent element you can ignore `width` and `height` props or set it to `auto`. |
+| active               | number    | middle child node | The index of the image you want to be active.|
+| clickable            | boolean   | true              | Makes images clickable, setting to false will disable clickability of images. |
+| currentFigureScale   | number    | 1.5               | The scale factor (float) to be applied on the active image. |
+| otherFigureScale     | number    | .8                | The scale factor (float) to be applied on the inactive images.|
+| classes              | object    |                   | To support style changes for customization. |
+| className            | string    |                   | To add css class to the coverflow to customize. |
+
+#### Responsive
+
+Now, you can use `media` props to support responsive design. The syntax part is come form [Radium](http://projects.formidablelabs.com/radium/)
+You can reference to this [doc](https://github.com/FormidableLabs/radium/tree/master/docs/guides#media-queries).
+
+* 2016-09-13 If you need RWD you should add `<StyleRoot>`
+
+```js
+ReactDOM.render(
+  <StyleRoot>
+    <Coverflow
+      displayQuantityOfSide={2}
+      navigation={true}
+      enableHeading={true}
+      active={0}
+      media={{
+        '@media (max-width: 900px)': {
+          width: '600px',
+          height: '300px'
+        },
+        '@media (min-width: 900px)': {
+          width: '960px',
+          height: '600px'
+        }
+      }}
+      >
+      <img src='images/album-1.png' alt='Album one' data-action="https://facebook.github.io/react/"/>
+      <img src='images/album-2.png' alt='Album two' data-action="http://passer.cc"/>
+      <img src='images/album-3.png' alt='Album three' data-action="https://doce.cc/"/>
+      <img src='images/album-4.png' alt='Album four' data-action="http://tw.yahoo.com"/>
+    </Coverflow>
+  </StyleRoot>
+  ,
+  document.querySelector('.example_2')
+);
+```
+
+## Contributors
+
+* [andyyou](https://github.com/andyyou)
+* [Calvert Yang](https://github.com/CalvertYang)
+* [Ariel Salem](https://github.com/asalem1)
+
+Use [Github issues](https://github.com/andyyou/react-coverflow/issues) for requests
+
+`react-coverflow` is a community project and wouldn't be what it is without contributions! We encourage and support contributions. The `react-coverflow` source code is released under the MIT License.
+
+Feel free to fork and improve/enhance `react-coverflow` any way you want. If you feel that `react-coverflow` will benefit from your changes, please open a pull request.
+
+## Development
+
+If you want to custom the component there are some commands that can help you.
+
+```
+# Compile component to dist/
+$ npm run build-npm
+
+# Build source of site
+$ npm run build-site
+
+# Clean
+$ npm run clean
+
+# Development Server
+$ npm run dev
+
+# Deploy examples to gh-pages
+$ npm run build-npm
+$ npm run build-site
+$ git subtree push --prefix site origin gh-pages
+$ npm version patch
+$ npm publish
+```
+
+## Release History
+* 2019-04-13
+  - Removed button navigation for arrow navigation. Using arrow navigation now disables the image-click navigation that had previously existed.
+* 2018-03-04
+  - Added Infinite Scrolling Option that allows users to scroll from first to last option continuously using the arrow buttons. Buttons dynamically rendered based on current position of img
+* 2018-02-24
+  - Fixed deprecation errors / warnings, updated packages.
+* 2016-09-29
+  - Add `clickable` props to on/off click of image.
+* 2016-09-22
+  - Fix issue change `active` can update `current` image.
+* 2016-09-16
+  - Add keypress events for prev/next
+* 2016-09-13
+  - Fix [Unknown Prop Warning](https://facebook.github.io/react/warnings/unknown-prop.html)
+  - Radium updated that if you need RWD feature you need to wrap `<StyleRoot>`
+  - Remove img `url` attribute, use `data-action` instead. when value is string will redirectin url otherwise can put function will be execute.
+  - Add `active` attribute
+* 2016-09-10
+  - Use `postcss-loader` instead of `autoprefixer-loader`
+  - Update babel to v6+
+* 2015-12-12 Fix React 0.14.3 two copies issue
+* 2015-12-10 Update React version.
+* 2015-11-13
+  - v0.1.9 Support responsive props.
+  - Update eslint with babel-eslint.
+* 2015-11-11
+  - v0.1.8 Fix some issues and add option to enable/disable scroll ability.
+  - Add displayName to fix react-devtool show t tag.
+* 2015-11-10
+  - v0.1.6 Support react 0.14.2
+* 2015-10-09
+  - v0.1.4 Add propTypes and enableHeading options for mobile.
+  - Add example of responsive.
+* 2015-10-07 v0.1.3 Support even number of images, Fix bugs
+* 2015-09-22 v0.1.0
+
+
+## Issues
+
+* If you get the errors as follow
+
+```
+Warning: Any use of a keyed object should be wrapped in React.addons.createFragment(object) before being passed as a child. Warning: t(...): React component classes must extend React.Component.
+```
+
+That because of React 0.14.2 got some change and the component not update before.
+
+* [addCSS issue](https://github.com/FormidableLabs/radium/issues/574)
+
+```
+Error: To use plugins requiring addCSS (e.g. keyframes, media queries), please wrap your application in the StyleRoot component
+```
+
+
+## Todo
+
+* Write test case
+* Add further customization options
+* Add Prev/Next Arrows instead of Buttons
---
+++ src/Coverflow.js
@@ -0,0 +1,411 @@
+/* eslint-disable */
+/**
+ * React Coverflow
+ *
+ * Author: andyyou & asalem1
+ */
+import React, { Component, createRef } from 'react';
+import PropTypes from 'prop-types';
+import Radium, { StyleRoot } from 'radium';
+import styles from './stylesheets/coverflow.scss';
+
+const TOUCH = {
+  move: false,
+  lastX: 0,
+  sign: 0,
+  lastMove: 0,
+};
+
+const TRANSITIONS = [
+  'transitionend',
+  'oTransitionEnd',
+  'otransitionend',
+  'MSTransitionEnd',
+  'webkitTransitionEnd',
+];
+
+const HandleAnimationState = function() {
+  this._removePointerEvents();
+};
+
+class Coverflow extends Component {
+  /**
+   * Life cycle events
+   */
+  refNode = createRef();
+
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    displayQuantityOfSide: PropTypes.number.isRequired,
+    navigation: PropTypes.bool,
+    enableHeading: PropTypes.bool,
+    enableScroll: PropTypes.bool,
+    clickable: PropTypes.bool,
+    currentFigureScale: PropTypes.number,
+    otherFigureScale: PropTypes.number,
+    active: PropTypes.number,
+    media: PropTypes.any,
+    classes: PropTypes.object,
+    className: PropTypes.string,
+    infiniteScroll: PropTypes.bool,
+    width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  };
+
+  static defaultProps = {
+    navigation: false,
+    enableHeading: true,
+    enableScroll: true,
+    clickable: true,
+    classes: {},
+    className: '',
+    currentFigureScale: 1.5,
+    otherFigureScale: 0.8,
+    active: 0,
+    media: {},
+    infiniteScroll: false,
+    width: 'auto',
+    height: 'auto',
+  };
+
+  state = {
+    current: ~~(React.Children.count(this.props.children) / 2),
+    move: 0,
+    width: this.props.width,
+    height: this.props.height,
+  };
+
+  componentDidMount() {
+    this.updateDimensions(this.props.active);
+    const length = React.Children.count(this.props.children);
+
+    TRANSITIONS.forEach(event => {
+      for (let i = 0; i < length; i++) {
+        const figureID = `figure_${i}`;
+        this.refs[figureID].addEventListener(event, HandleAnimationState.bind(this));
+      }
+    });
+
+    const eventListener = window && window.addEventListener;
+
+    if (eventListener) {
+      window.addEventListener('resize', this.updateDimensions.bind(this));
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.active !== prevProps.active) {
+      this.updateDimensions(this.props.active);
+    }
+  }
+
+  componentWillUnmount() {
+    const length = React.Children.count(this.props.children);
+
+    TRANSITIONS.forEach(event => {
+      for (let i = 0; i < length; i++) {
+        const figureID = `figure_${i}`;
+        this.refs[figureID].removeEventListener(event, HandleAnimationState.bind(this));
+      }
+    });
+
+    // const removeListener = window && window.removeEventListener;
+
+    // if(removeListener) {
+    //   window.removeEventListener('resize', this.updateDimensions.bind(this));
+    // }
+  }
+
+  updateDimensions(active) {
+    const { displayQuantityOfSide } = this.props;
+    const length = React.Children.count(this.props.children);
+    const center = this._center();
+    let state = {
+      width: this.refNode.current.offsetWidth,
+      height: this.refNode.current.offsetHeight,
+    };
+    const baseWidth = state.width / (displayQuantityOfSide * 2 + 1);
+    let activeImg = typeof active === 'number' ? active : this.props.active;
+    if (typeof active === 'number' && ~~active < length) {
+      activeImg = ~~active;
+      let move = 0;
+      move = baseWidth * (center - activeImg);
+
+      state = Object.assign({}, state, {
+        current: active,
+        move,
+      });
+    }
+    this.setState(state);
+  }
+
+  render() {
+    const { enableScroll, navigation, className, classes, infiniteScroll, media } = this.props;
+    const { width, height, current } = this.state;
+    const renderPrevBtn = infiniteScroll ? true : current > 0;
+    const renderNextBtn = infiniteScroll ? true : current < this.props.children.length - 1;
+    return (
+      <div
+        ref={this.refNode}
+      >
+        <StyleRoot>
+          <div
+            className={`${className} ${styles.container}`}
+            style={
+              Object.keys(media).length !== 0 ? media : { ...classes, width: `${width}px`, height: `${height}px` }
+        }
+            onWheel={enableScroll ? this._handleWheel.bind(this) : null}
+            onTouchStart={this._handleTouchStart.bind(this)}
+            onTouchMove={this._handleTouchMove.bind(this)}
+            onKeyDown={this._keyDown.bind(this)}
+            tabIndex="-1"
+          >
+            <div className={styles.coverflow}>
+              <div className={styles.preloader} />
+              <div className={styles.stage} ref="stage">
+                {navigation && (
+                  <div
+                    id={styles.arrow1}
+                    className={styles.arrowWrapper}
+                  >
+                    {renderPrevBtn && (
+                      <div
+                        onClick={(e) => this._handlePrevFigure(e)}
+                        className={`${styles.arrow} ${styles.left}`}
+                      >
+                        <span/>
+                      </div>
+                    )}
+                    {this._renderFigureNodes()}
+                    {renderNextBtn && (
+                      <div
+                        onClick={(e) => this._handleNextFigure(e)}
+                        className={`${styles.arrow} ${styles.right}`}
+                      >
+                        <span/>
+                      </div>
+                    )}
+                  </div>
+                )}
+                {!navigation && this._renderFigureNodes()}
+              </div>
+              </div>
+          </div>
+        </StyleRoot>
+      </div>
+    );
+  }
+
+  /**
+   * Private methods
+   */
+  _center() {
+    const length = React.Children.count(this.props.children);
+    return ~~(length / 2);
+  }
+
+  _keyDown(e) {
+    if (e.keyCode === 37) {
+      this._handlePrevFigure();
+    } else if (e.keyCode === 39) {
+      this._handleNextFigure();
+    }
+  }
+
+  _handleFigureStyle(index, current) {
+    const { displayQuantityOfSide, navigation } = this.props;
+    const { width } = this.state;
+    const style = {};
+    const baseWidth = width / (displayQuantityOfSide * 2 + 1);
+    const length = React.Children.count(this.props.children);
+    const offset = length % 2 === 0 ? -width / 10 : 0;
+    // Handle opacity
+    const depth = displayQuantityOfSide - Math.abs(current - index);
+    let opacity = depth === 1 ? 0.95 : 0.5;
+    opacity = depth === 2 ? 0.92 : opacity;
+    opacity = depth === 3 ? 0.9 : opacity;
+    opacity = current === index ? 1 : opacity;
+    // Handle translateX
+    if (index === current) {
+      style.width = `${baseWidth}px`;
+      style.transform = `translateX(${this.state.move + offset}px)  scale(${
+        this.props.currentFigureScale
+      }`;
+      style.zIndex = `${10 - depth}`;
+      style.opacity = opacity;
+    } else if (index < current) {
+      // Left side
+      style.width = `${baseWidth}px`;
+      style.transform = `translateX(${this.state.move + offset}px) rotateY(40deg) scale(${
+        this.props.otherFigureScale
+      }`;
+      style.zIndex = `${10 - depth}`;
+      style.opacity = opacity;
+      if (navigation) {
+        style.pointerEvents = 'none';
+      }
+    } else if (index > current) {
+      // Right side
+      style.width = `${baseWidth}px`;
+      style.transform = ` translateX(${this.state.move + offset}px) rotateY(-40deg) scale(${
+        this.props.otherFigureScale
+      })`;
+      style.zIndex = `${10 - depth}`;
+      style.opacity = opacity;
+      if (navigation) {
+        style.pointerEvents = 'none';
+      }
+    }
+    return style;
+  }
+
+  _handleFigureClick = (index, action, e) => {
+    if (!this.props.clickable) {
+      e.preventDefault();
+      return;
+    }
+    if (this.state.current === index) {
+      // If on the active figure
+      if (typeof action === 'string') {
+        // If action is a URL (string), follow the link
+        window.open(action, '_blank');
+      }
+
+      this._removePointerEvents();
+    } else {
+      // Move to the selected figure
+      e.preventDefault();
+      const { displayQuantityOfSide } = this.props;
+      const { width } = this.state;
+      const baseWidth = width / (displayQuantityOfSide * 2 + 1);
+      const distance = this._center() - index;
+      const move = distance * baseWidth;
+      this.setState({ current: index, move });
+    }
+  };
+
+  _renderFigureNodes = () => {
+    const { enableHeading } = this.props;
+    const { current } = this.state;
+    const figureNodes = React.Children.map(this.props.children, (child, index) => {
+      const figureElement = React.cloneElement(child, {
+        className: styles.cover,
+      });
+      const style = this._handleFigureStyle(index, current);
+      return (
+        <figure
+          className={styles.figure}
+          key={index}
+          onClick={e => this._handleFigureClick(index, figureElement.props['data-action'], e)}
+          style={style}
+          ref={`figure_${index}`}
+        >
+          {figureElement}
+          {enableHeading && <div className={styles.text}>{figureElement.props.alt}</div>}
+        </figure>
+      );
+    });
+    return figureNodes;
+  };
+
+  _removePointerEvents() {
+    this.refs.stage.style.pointerEvents = 'auto';
+  }
+
+  _hasPrevFigure = () => this.state.current - 1 >= 0;
+
+  _hasNextFigure = () => this.state.current + 1 < this.props.children.length;
+
+  _handlePrevFigure = (e) => {
+    const { displayQuantityOfSide, infiniteScroll } = this.props;
+    const { width } = this.state;
+    const { current } = this.state;
+    const baseWidth = width / (displayQuantityOfSide * 2 + 1);
+    const distance =
+      this._center() - (current - 1 < 0 ? this.props.children.length - 1 : current - 1);
+    const move = distance * baseWidth;
+
+    if (current - 1 >= 0) {
+      this.setState({ current: current - 1, move });
+      TOUCH.lastMove = move;
+    }
+    if (current - 1 < 0 && infiniteScroll) {
+      this.setState({ current: this.props.children.length - 1, move });
+      TOUCH.lastMove = move;
+    }
+  };
+
+  _handleNextFigure = (e) => {
+    const { displayQuantityOfSide, infiniteScroll } = this.props;
+    const { width } = this.state;
+    const { current } = this.state;
+    const baseWidth = width / (displayQuantityOfSide * 2 + 1);
+    const distance = this._center() - (current + 1 >= this.props.children.length ? 0 : current + 1);
+    const move = distance * baseWidth;
+
+    if (current + 1 < this.props.children.length) {
+      this.setState({ current: current + 1, move });
+      TOUCH.lastMove = move;
+    }
+    if (current + 1 >= this.props.children.length && infiniteScroll) {
+      this.setState({ current: 0, move });
+      TOUCH.lastMove = move;
+    }
+  };
+
+  _handleWheel(e) {
+    const delta = Math.abs(e.deltaY) === 125 ? e.deltaY * -120 : e.deltaY < 0 ? -600000 : 600000;
+    const count = Math.ceil(Math.abs(delta) / 120);
+
+    if (count > 0) {
+      const sign = Math.abs(delta) / delta;
+      let func = null;
+
+      if (sign > 0 && this._hasPrevFigure()) {
+        e.preventDefault();
+        func = this._handlePrevFigure();
+      } else if (sign < 0 && this._hasNextFigure()) {
+        e.preventDefault();
+        func = this._handleNextFigure();
+      }
+
+      if (typeof func === 'function') {
+        for (let i = 0; i < count; i++) func();
+      }
+    }
+  }
+
+  _handleTouchStart(e) {
+    TOUCH.lastX = e.nativeEvent.touches[0].clientX;
+    TOUCH.lastMove = this.state.move;
+  }
+
+  _handleTouchMove(e) {
+    e.preventDefault();
+    const { displayQuantityOfSide } = this.props;
+    const { width } = this.state;
+
+    const clientX = e.nativeEvent.touches[0].clientX;
+    const lastX = TOUCH.lastX;
+    const baseWidth = width / (displayQuantityOfSide * 2 + 1);
+    const move = clientX - lastX;
+    const totalMove = TOUCH.lastMove - move;
+    const sign = Math.abs(move) / move;
+
+    if (Math.abs(totalMove) >= baseWidth) {
+      let fn = null;
+      if (sign > 0) {
+        fn = this._handlePrevFigure();
+      } else if (sign < 0) {
+        fn = this._handleNextFigure();
+      }
+      if (typeof fn === 'function') {
+        fn();
+      }
+    }
+  }
+}
+
+Coverflow.displayName = 'Coverflow';
+
+export default Radium(Coverflow);
